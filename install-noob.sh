1df3a1c37c2919ff5c7254888b084e9f66feec84
KozzX <andre.candido@MOLD2016.local>
2017-09-25 08:31:09 -0300

    Adicionado instalador

--- install-noob.sh
+++ install-noob.sh
@@ -1,5 +1,11 @@
 echo Atualizando repositórios..
-echo Atualizando repositórios..
-echo Atualizando repositórios..
-echo Atualizando repositórios..
-echo Atualizando repositórios..
+
+cd ~;
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh;
+sudo bash nodesource_setup.sh;
+sudo apt-get install nodejs;
+sudo apt-get install build-essential;
+git clone https://github.com/KozzX/NoobHub/;
+sudo npm install -g pm2;
+pm2 start NoobHub/server/node.js;
+pm2 monit;
